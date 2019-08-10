983d8cca7bcf584c5652a3057b439885786a6492
Éric Lemoine <eric.lemoine@oslandia.com>
2019-08-10 21:45:50 +0200

    Improve the create-usb-key.sh script

--- tools/create-usb-key.sh
+++ tools/create-usb-key.sh
@@ -5,6 +5,8 @@ set -e
 DEV=/dev/sdb
 PART=${DEV}1
 MNTPT=/media/usb
+OVAFILE=workshop-qwc2.ova
+MD5FILE=${OVAFILE}.md5
 
 while /bin/true; do
     read -p "create key? [yes|no] " yn
@@ -13,11 +15,18 @@ while /bin/true; do
         exit
         ;;
     [Yy]* ) 
-        echo "type=7" | sudo sfdisk $DEV
-        sudo mkfs.exfat $PART
+        echo "create partition..."
+        echo "type=7" | sudo sfdisk $DEV > /dev/null
+        echo "create file system..."
+        sudo mkfs.exfat $PART > /dev/null
+        echo "mount device..."
         sudo mount $PART $MNTPT
-        cp workshop-qwc2.ova ${MNTPT}/
-        sudo umount $PART
+        echo "copy files to device..."
+        time cp $OVAFILE $MD5FILE $MNTPT/
+        echo "check md5..."
+        md5sum -c $MNTPT/$MD5FILE
+        echo "unmount device..."
+        sudo umount $MNTPT
         ;;
     esac
 done
